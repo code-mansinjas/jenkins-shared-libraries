c78f90dff7b9b42709fbe9b04515e50a9da2673f
code.mansinjas <code.mansinjas@gmail.com>
2024-10-19 20:13:08 +0530

    Corrected groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -2,7 +2,7 @@ def call(String CredentialsId, String ProjectName, String ImageTag, String Usern
     echo "Pushing Image to Docker Hub Started =========================>>>>>>>>>>>>>>>>>>>>>>>>>> "
     withCredentials([usernamePassword(credentialsId:"${CredentialsId}", passwordVariable: "${PasswordVariable}", usernameVariable: "${UsernameVariable}")]){
         echo "Values ==>> ${env}"
-        sh "docker login -u ${env["${UsernameVariable}"]} -p ${env.["${PasswordVariable}"]}"
+        sh "docker login -u ${env["${UsernameVariable}"]} -p ${env["${PasswordVariable}"]}"
         sh "docker image tag ${ProjectName}:${ImageTag} ${env["${UsernameVariable}"]}/${ProjectName}:${ImageTag}"
         sh "docker push ${env["${UsernameVariable}"]}/${ProjectName}:${ImageTag}"
     }
